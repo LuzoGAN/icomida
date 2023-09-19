b2b89db04c4cf43720aeed7e58ed4d9a0cec7c3c
luzo <luzoaki@gmail.com>
2023-09-19 18:45:51 -0300

    Identação

--- appmenu.py
+++ appmenu.py
@@ -1,15 +1,14 @@
 from flet import *
-from flet import colors
 
 jumbotron = AppBar(
     toolbar_height=90,
     bgcolor="#E3002A",
     title=Row([
         Container(
-            padding = padding.all(4),
-            border = border.all(3, "white"),
-            border_radius = border_radius.all(30),
-            content= Text("Localização", color="white", size=20)
+            padding=padding.all(4),
+            border=border.all(3, "white"),
+            border_radius=border_radius.all(30),
+            content=Text("Localização", color="white", size=20)
         ),
         Container(
             content=Image(
--- main.py
+++ main.py
@@ -4,26 +4,28 @@ from appmenu import jumbotron
 from cardsection import sectioncard
 from tabsmenu import tabs_menu
 
-def main(page:Page):
-	page.padding = 0
-	page.spacing = 0
-	page.add(
-		# APPMENU
-		jumbotron,
-		# SECTION CARD MENU
-		sectioncard,
 
-		# FOR TABS SECTION
-	Container(
-	border_radius = border_radius.only(),
-	# for GET FULL WIDTH
-	width=page.window_width,
-	bgcolor="white",
-	# THIS CONTENT
-	content = tabs_menu
+def main(page: Page):
+    page.padding = 0
+    page.spacing = 0
+    page.add(
+        # APPMENU
+        jumbotron,
+        # SECTION CARD MENU
+        sectioncard,
 
-		)
+        # FOR TABS SECTION
+        Container(
+            border_radius=border_radius.only(),
+            # for GET FULL WIDTH
+            width=page.window_width,
+            bgcolor="white",
+            # THIS CONTENT
+            content=tabs_menu
 
-		)
+        )
+
+    )
+
 
 flet.app(target=main, assets_dir="assets")
